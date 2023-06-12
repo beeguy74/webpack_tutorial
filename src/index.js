82362edc08c0bde674a7a1c90f680b6a63f2c31f
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-06-12 19:36:17 +0200

    background icon set

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 
 function component() {
@@ -8,6 +9,11 @@ function component() {
   // Lodash, now imported
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
