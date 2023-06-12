98fae91c1d3bf204d452597fedc9f8987eb6388a
Iaroslav NEVEROV <beeguy74@gmail.com>
2023-06-12 19:56:43 +0200

    print btn

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
 function componentDefault() {
   const element = document.createElement('div');
 
-  // Lodash, now imported
   element.innerHTML = _.join(['I', 'am', 'default'], ' ');
   return element;
 }
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
+  element.appendChild(btn);
   return element;
 }
 
